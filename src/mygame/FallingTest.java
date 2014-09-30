f2b550c78c45bb22f4cb415c01db1621fe408332
SavDont <sav.dontamsetti@gmail.com>
2014-09-29 23:04:04 -0400

    Added New Class
    Made a new class which has cube fall in space

---
+++ src/mygame/FallingTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+/**
+ *
+ * @author krish
+ */
+public class FallingTest extends SimpleApplication {
+
+    protected Geometry player;
+    protected Material mat;
+
+    public static void main(String[] args) {
+        FallingTest app = new FallingTest();
+        app.start();
+    }
+
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1);
+        player = new Geometry("Blue Cube", b);
+        mat = new Material (assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Gray);
+        player.setMaterial(mat);
+        rootNode.attachChild(player);
+    }
+    protected boolean grow = true;
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        player.move(0,-2*tpf,0);
+    }
+}
