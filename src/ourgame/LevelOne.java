15e024ee95e5f069565f664d1e0e9a8697aff39e
Ryan Miller <ryan.miller4224@gmail.com>
2014-10-06 21:35:27 -0400

    Implemented Physics
    It's F#%ing Awesome! :panda_face:

--- src/ourgame/LevelOne.java
+++ src/ourgame/LevelOne.java
@@ -5,6 +5,14 @@
 package ourgame;
 import com.jme3.util.SkyFactory;
 import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.BoxCollisionShape;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.BetterCharacterControl;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.input.ChaseCamera;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.AnalogListener;
@@ -13,8 +21,6 @@ import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.FastMath;
-import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.CameraNode;
 import com.jme3.scene.Node;
@@ -27,11 +33,9 @@ public class LevelOne extends SimpleApplication{
         LevelOne app = new LevelOne();
         app.start();
     }
-    private Spatial player;
-    
-    double g = -9.8;
-    double yVel = 0;
-    int terminalVel = 5;
+    private BulletAppState bulletAppState;
+    private RigidBodyControl terrainControl;
+    private RigidBodyControl playerControl;
     
     public void simpleInitApp()
     {   
@@ -51,12 +55,36 @@ public class LevelOne extends SimpleApplication{
         sun.setDirection(new Vector3f(0,-1,0));
         rootNode.addLight(sun);
         
+        
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        
+        Spatial terrain = assetManager.loadModel("Models/Terrain/Terrain.j3o");
+        Material theGround = new Material (assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        theGround.setTexture("DiffuseMap", assetManager.loadTexture("Textures/GroundStuffs.png"));
+        terrain.setMaterial(theGround);
+        terrain.setLocalScale(new Vector3f(1000f, 1000f, 1000f));
+        
+        CollisionShape terrainShape = CollisionShapeFactory.createMeshShape(terrain);
+        terrainControl = new RigidBodyControl(terrainShape, 0);
+        terrain.addControl(terrainControl);
+        rootNode.attachChild(terrain);
+        
+        
         Node pNode = new Node("pNode");
+        Spatial player;
         player = assetManager.loadModel("Models/Dude/WIP Dude Frame.obj");
         Material skinAndClothes = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
         player.setMaterial(skinAndClothes);
-        
+        player.setLocalTranslation(0, 1000, 0);
         player.rotate(135, 0, -90);
+        
+        CapsuleCollisionShape PlayerShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+        playerControl = new RigidBodyControl(PlayerShape, .05f);
+        playerControl.setGravity(new Vector3f(0f, 9.8f, 0f));
+        //playerControl.setPhysicsLocation(new Vector3f(0f, 10000f, 0f));
+        player.addControl(playerControl);
+        
         pNode.attachChild(player);
         
         CameraNode camNode = new CameraNode("Camera Node", cam);
@@ -73,24 +101,13 @@ public class LevelOne extends SimpleApplication{
         
         rootNode.attachChild(pNode);
         
+        bulletAppState.getPhysicsSpace().add(terrainControl);
+        bulletAppState.getPhysicsSpace().add(playerControl);
         
-        Spatial terrain = assetManager.loadModel("Models/Terrain/Terrain.j3o");
-        Material theGround = new Material (assetManager, "Common/MatDefs/Light/Lighting.j3md");
-        theGround.setTexture("DiffuseMap", assetManager.loadTexture("Textures/GroundStuffs.png"));
-        terrain.setMaterial(theGround);
-        terrain.setLocalScale(new Vector3f(500f,200f,500f));
-        terrain.setLocalTranslation(new Vector3f(0,-900,0));
-        rootNode.attachChild(terrain);
         initKeys();
     }
-    @Override
-    public void simpleUpdate (float tpf)
-    {
-        yVel += g*tpf;
-        if(yVel > terminalVel)
-            yVel = terminalVel;
-        player.move(0,(float)(yVel*tpf),0);
-    }
+
+    
     private void initKeys()
     {
         inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_UP));
@@ -104,22 +121,21 @@ public class LevelOne extends SimpleApplication{
     {
         public void onAnalog(String name, float value, float tpf)
         {
-            Vector3f v = player.getLocalTranslation();
             if (name.equals("Right"))
             {
-                player.setLocalTranslation(v.x, v.y,v.z-50*tpf);
+                playerControl.applyCentralForce(new Vector3f(0, 0, -2));
             }     
             else if (name.equals("Up"))
             {
-                player.setLocalTranslation(v.x-50*tpf,v.y,v.z);
+                playerControl.applyCentralForce(new Vector3f(-2, 0, 0));
             }
             else if (name.equals("Left"))
             {
-                player.setLocalTranslation(v.x,v.y,v.z+50*tpf);
+                playerControl.applyCentralForce(new Vector3f(0, 0, 2));
             }
             else if (name.equals("Down"))
             {
-                player.setLocalTranslation(v.x+50*tpf,v.y, v.z);
+                playerControl.applyCentralForce(new Vector3f(2, 0, 0));
             }
         }
     };
