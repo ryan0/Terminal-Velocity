18875915e26e32a2ccdfbb25bb39dd9eaf1a1e86
SavDont <sav.dontamsetti@gmail.com>
2014-11-24 21:36:22 -0500

    Added Coin class
    Added a coin class with ghost control. Still need model to add to class.
    Currently coin does not work.

--- src/ourgame/Coin.java
+++ src/ourgame/Coin.java
@@ -3,28 +3,21 @@
  * and open the template in the editor.
  */
 package ourgame;
-
 import com.jme3.app.Application;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.collision.shapes.CollisionShape;
-import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.control.GhostControl;
 import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.material.Material;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.util.TangentBinormalGenerator;
-
-
 /**
- * A node extension that contains a <code>mesh</code>
- * for the terrain and its specifications.
- * 
- * @author Ryan
+ *
+ * @author SD
  */
-public class Terrain extends Node
-{
-    private RigidBodyControl physicsControl;
+public class Coin extends Node{
     private Spatial mesh;
     
     /**
@@ -34,23 +27,25 @@ public class Terrain extends Node
      * @param bulletAppState the desired parent bulletAppState
      * @param app the desired parent Application
      */
-    public Terrain(BulletAppState bulletAppState, Application app)
+    public Coin(BulletAppState bulletAppState, Application app, Vector3f position,Vector3f size)
     {
-        setName("Terrain");
-        mesh = app.getAssetManager().loadModel("Models/Terrain/terrain.obj");
+        setName("Coin");
+        //mesh = app.getAssetManager().loadModel("insert coin stuffs here");
 
+        //note: the bottom stuff has been copied and pasted from terrain.java, waiting for the models to be finished and the texture
         TangentBinormalGenerator.generate(mesh);
         Material mat = new Material (app.getAssetManager(), "Common/MatDefs/Light/Lighting.j3md");
         mat.setTexture("DiffuseMap", app.getAssetManager().loadTexture("Textures/TerrainTexture.png"));
         mat.setTexture("NormalMap", app.getAssetManager().loadTexture("Textures/TerrainNormalMap.png"));
         mesh.setMaterial(mat);
-        mesh.setLocalScale(new Vector3f(100000f, 100000f, 100000f));
+        mesh.setLocalScale(size);
+        mesh.setLocalTranslation(position);
 
-        
-        CollisionShape terrainShape = CollisionShapeFactory.createMeshShape(mesh);
-        physicsControl = new RigidBodyControl(terrainShape, 0);
-        mesh.addControl(physicsControl);
+        CollisionShape coinShape = CollisionShapeFactory.createMeshShape(mesh);
+        GhostControl ghost = new GhostControl(coinShape);
+        mesh.addControl(ghost);
         this.attachChild(mesh);
         bulletAppState.getPhysicsSpace().add(mesh);
     }
+    
 }
